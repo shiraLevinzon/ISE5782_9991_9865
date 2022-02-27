181e2c4407e4093ea5c2db1608f893c7a5b3b2ce
שירה לוינזון <levinzon@g.jct.ac.il>
2022-02-27 15:57:45 +0200

    15:57

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -6,7 +6,6 @@ public class Point {
 	  * constructor that receiving the values of the three coordinates from Double
 	  * constructors that receiving Object from Double3 type
 	  * @param xyz
-	  * 
 	  */
 		 
 	public Point(Double3 xyz) {
@@ -16,8 +15,10 @@ public class Point {
 	public Point(double d1,double d2,double d3) {
 		this.xyz=new Double3(d1,d2,d3);
 	}
-	//Override equals func that compare between two points and return true if they equal,otherwise כשךדק
 	
+	/**
+	 * Override equals func that compare between two points and return true if they equal,otherwise false
+	 */
 	@Override
 	  public boolean equals(Object obj) {
 	      if (this == obj) return true;
@@ -26,24 +27,58 @@ public class Point {
 	      Point other = (Point)obj;
 	      return this.xyz.equals(other.xyz);
 	   }
-	 // Override to string func
+	/**
+	 * Override to string func
+	 * @return String
+	 */ 
 	@Override
 	public String toString() {
 		return "Point [xyz=" + xyz.toString() + "]";
 	}
-	//Get Func
-	public Double3 getXyz() {
+	/**
+	 * Get Func
+	 * @return Double3
+	 */
+	/*public Double3 getXyz() {
 		return xyz;
-	}
+	}*/
 	
+	/**
+	 * Sum two floating point triads into a new triad where each couple of numbers
+	 * is summarized
+	 * 
+	 * @param rhs right handle side operand for addition
+	 * @return result of add
+	 */
 	Point add(Vector v) {
-		return new Point(v.getXyz().add(this.xyz));
+		return new Point(v.xyz.add(this.xyz));
 	}
-	
-	Vector subtract(Point rhs) {
-		return new Vector();
+	/**
+	 * Subtract two floating point triads into a new triad where each couple of
+	 * numbers is subtracted
+	 * 
+	 * @param rhs right handle side operand for addition
+	 * @return result of add
+	 */
+	Vector subtract(Point p) {
+		return new Vector(p.xyz.subtract(this.xyz));
 	}
-	
+	/**
+	 * @param p- point
+	 * @return the squared distance between two points 
+	 */
+	double DistanceSquared(Point p) {
+		Point po=new Point(p.xyz.subtract(this.xyz));
+		return po.xyz.d1*po.xyz.d1 + po.xyz.d2*po.xyz.d2 + po.xyz.d3*po.xyz.d3;
+	}
+	/**
+	 * @param p- point
+	 * @return the distance between two points 
+	 */
+	 double Distance(Point p) {
+		 return Math.sqrt(DistanceSquared(p));
+	 }
+	 
 	
 
 }
