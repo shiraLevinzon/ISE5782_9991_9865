92776b99dcc373f2ab0494883153d0b0bf77223f
שירה לוינזון <levinzon@g.jct.ac.il>
2022-02-27 15:54:36 +0200

    Tar 2 push number 2

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,5 +1,49 @@
 package primitives;
 
 public class Point {
+	 Double3 xyz;
+	 /**constructors
+	  * constructor that receiving the values of the three coordinates from Double
+	  * constructors that receiving Object from Double3 type
+	  * @param xyz
+	  * 
+	  */
+		 
+	public Point(Double3 xyz) {
+		this.xyz=new Double3(xyz.d1,xyz.d2,xyz.d3);
+	}
+
+	public Point(double d1,double d2,double d3) {
+		this.xyz=new Double3(d1,d2,d3);
+	}
+	//Override equals func that compare between two points and return true if they equal,otherwise כשךדק
+	
+	@Override
+	  public boolean equals(Object obj) {
+	      if (this == obj) return true;
+	      if (obj == null) return false;
+	      if (!(obj instanceof Point)) return false;
+	      Point other = (Point)obj;
+	      return this.xyz.equals(other.xyz);
+	   }
+	 // Override to string func
+	@Override
+	public String toString() {
+		return "Point [xyz=" + xyz.toString() + "]";
+	}
+	//Get Func
+	public Double3 getXyz() {
+		return xyz;
+	}
+	
+	Point add(Vector v) {
+		return new Point(v.getXyz().add(this.xyz));
+	}
+	
+	Vector subtract(Point rhs) {
+		return new Vector();
+	}
+	
+	
 
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,5 +1,23 @@
 package primitives;
 
-public class Vector {
+public class Vector extends Point {
+
+	public Vector(double d1, double d2, double d3) {
+		super(d1, d2, d3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Vector(Double3 xyz) {
+		super(xyz);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Vector- " + super.toString();
+	}
+
+	
+	
 
 }
