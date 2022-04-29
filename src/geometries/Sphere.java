2de5fbe70777f859596736c4c52e99a0b3e1da93
Abigailuzan <80058360+Abigailuzan@users.noreply.github.com>
2022-04-29 04:26:32 +0300

    dbf

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -1,48 +1,97 @@
 package geometries;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import primitives.Point;
 import primitives.Ray;
+import primitives.Util;
 import primitives.Vector;
 
-public class Sphere implements Geometry  {
-	private Double radius;
-	private Point center;
+public class Sphere implements Geometry {
 
+	private Point center;
+	private double radius;
+
+	/**
+	 * getter center of sphere
+	 * 
+	 * @return a center of sphere
+	 */
+	public Point getCenter() {
+		return center;
+	}
+
+	/**
+	 * getter radius of sphere
+	 * 
+	 * @return a radius of sphere
+	 */
+	public double getRadius() {
+		return radius;
+	}
+
+	/**
+	 * Sphere constructor receiving a Point3d value to center and radius of Sphere
+	 * 
+	 * @param center - a center of sphere
+	 * @param radius - a radius of sphere
+	 */
+	public Sphere(Point center, double radius) {
+		this.center = center;
+		this.radius = radius;
+		//setMaxBoundary();
+		//setMinBoundary();
+	}
 
 	@Override
-	public Vector getNormal(Point p) {
-		// TODO Auto-generated method stub
+	public Vector getNormal(Point point) {
+		return point.subtract(center).normalize();
+	}
+
+	@Override
+	public String toString() {
+		return "center=" + center + ", radius=" + radius;
+	}
+
+	@Override
+	public List<Point> findIntsersections(Ray ray)  {
+		double tm;
+		double d;
+		var p0 = ray.getP0();
+		try {
+			var u = center.subtract(p0);
+			tm = ray.getDir().dotProduct(u);
+			d = Math.sqrt(u.lengthSquared() - (tm * tm));
+			if (d >= radius)
+				return null;
+		} catch (Exception e) {
+			d = 0;
+			tm = 0;
+		}
+		double th = Math.sqrt(radius * radius - (d * d));
+		double t1 = Util.alignZero(tm + th);
+		double t2 = Util.alignZero(tm - th);
+		double dis1 = Util.alignZero(t1 - max);
+		double dis2 = Util.alignZero(t2 - max);
+		Point p1, p2;
+		if (t1 > 0 && dis1 <= 0 || t2 > 0 && dis2 <= 0) {
+			List<Point> myList = new LinkedList<Point>();
+			if (t1 > 0 && dis1 <= 0) {
+				p1 = ray.getPoint(t1);
+				if (!p1.equals(p0))
+					myList.add(new Point(this.center, p1));
+			}
+			if (t2 > 0 && dis2 <= 0) {
+				p2 = ray.getPoint(t2);
+				if (!p2.equals(p0))
+					myList.add(new Point(this,p2));
+			}
+			return myList;
+		}
 		return null;
 	}
-	@Override
-	public List<Point> findIntsersections(Ray ray) {
-		//point and vector of ray
-		Point p0 = ray.getPoint();		//ray point
-		Vector v = ray.getDir();		//ray vector
-
-		if(p0.equals(center))       	//if the starting point of the ray is the center
-			return List.of(new Point(this,ray.getPoint(radius)));//return the intersection point
-		
-		Vector u=center.subtract(p0);	//the vector between center and ray
-		double tm=v.dotProduct(u); 		//the scale for the ray in order to get parallel to the sphere center
-		double d=Math.sqrt(u.lengthSquared()-tm*tm);//get the distance between the ray and the sphere center
-		//check if d is bigger then radius-the ray doesn't cross the sphere
-		if (d>radius)
-			return null;
-		double th=Math.sqrt(radius*radius-d*d);//according Pythagoras
-		double t1=tm+th;
-		double t2=tm-th;
-		if(t1>0&&t2>0&&!isZero(ray.getPoint(t1).subtract(center).dotProduct(v))&&!isZero(ray.getPoint(t2).subtract(center).dotProduct(v))) //if orthogonal -> no intersection
-			return List.of(new Point(this,ray.getPoint(t1)),new Point(this,ray.getPoint(t2)));
-		else if(t1>0&&!isZero(ray.getPoint(t1).subtract(center).dotProduct(v))) //if only t1 is not orthogonal and positive
-			return List.of(new Point(this,ray.getPoint(t1)));
-		else if(t2>0&&!isZero(ray.getPoint(t2).subtract(center).dotProduct(v))) //if only t2 is not orthogonal and positive
-			return List.of(new Point(this,ray.getPoint(t2)));
-		else
-			return null;//no intersections
-	}
+	
 }
 
 
--- src/unittests/SphereTests.java
+++ src/unittests/SphereTests.java
@@ -3,30 +3,137 @@
  */
 package unittests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import geometries.Sphere;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 
 /**
  * @author user
  *
  */
 class SphereTests {
+		/**
+		 * Test method for {@link geometries.Sphere#getNormal(primitives.Point3D)}.
+		 */
+		@Test
+		public void testGetNormal() {
+			// ============ Equivalence Partitions Tests ==============
+			var ball = new Sphere(new Point(0, 0, 0), 1);
 
-	/**
-	 * Test method for {@link geometries.Sphere#getNormal(primitives.Point)}.
-	 */
-	@Test
-	void testGetNormal() {
-		fail("Not yet implemented");
-	}
+			// Check that the normal is correct
+			assertEquals("getNormal(Point3D) -The normal to the Sphere is not correct ", new Vector(1, 0, 0),
+					ball.getNormal(new Point(1, 0, 0)));
+		}
 
-	/**
-	 * Test method for {@link geometries.Sphere#findIntsersections(primitives.Ray)}.
-	 */
-	@Test
-	void testFindIntsersections() {
-		fail("Not yet implemented");
+		/**
+		 * Test method for {@link geometries.Sphere#findIntersections(Ray)}.
+		 */
+		@Test
+		public void testFindIntersections() {
+			// ============ Equivalence Partitions Tests ==============
+			var sphere = new Sphere(new Point(1, 0, 0), 1);
+
+			// TC01: Ray's line is outside the sphere (0 points)
+			assertNull("Ray's line out of sphere",
+					sphere.findIntsersections(new Ray(new Vector(-1, 0, 0), new Point(1, 1, 0))));
+
+			// TC02: Ray starts before and crosses the sphere (2 points)
+			Point p1 = new Point(0.0651530771650466, 0.355051025721682, 0);
+			Point p2 = new Point(1.53484692283495, 0.844948974278318, 0);
+			List<Point> result = sphere.findIntsersections(new Ray(new Vector(-1, 0, 0), new Point(3, 1, 0)));
+
+			assertEquals("Wrong number of points", 2, result.size());
+			if (result.get(0).getX() > result.get(1).getX())
+				result = List.of(result.get(1), result.get(0));
+
+			assertEquals("Ray crosses sphere", List.of(p1, p2), result);
+
+			// TC03: Ray starts inside the sphere (1 point)
+			result = sphere.findIntsersections(new Ray(new Vector(1, 0.5, 0), new Point(1, 0, 0)));
+			assertEquals("Wrong number of points", 1, result.size());
+			assertEquals("Ray starts inside the sphere", new Point(1.8660254037844386, 0.5, 0), result.get(0));
+
+			// TC04: Ray starts after the sphere (0 points)
+			assertNull("Ray starts after the sphere and cross sphere ????",
+					sphere.findIntsersections(new Ray(new Vector(2, 0.5, 0), new Point(1, 0, 0))));
+
+			// =============== Boundary Values Tests ==================
+
+			// **** Group: Ray's line crosses the sphere (but not the center)
+
+			// TC11: Ray starts at sphere and goes inside (1 points)
+			result = sphere.findIntsersections(new Ray(new Vector(1, 0, 0),new Point(0.1339745962155614, 0.5, 0)));
+			assertEquals("Wrong number of points", 1, result.size());
+			assertEquals("Ray starts at sphere and goes inside and not cross sphere",
+					new Point(1.8660254037844386, 0.5, 0), result.get(0));
+
+			// TC12: Ray starts at sphere and goes outside (0 points)
+			assertNull("Ray starts at sphere and goes outside and cross sphere ????",
+					sphere.findIntsersections(new Ray(new Vector(-1, 0, 0),new Point(0.1339745962155614, 0.5, 0))));
+
+			// **** Group: Ray's line goes through the center
+			// TC13: Ray starts before the sphere (2 points)
+			p1 = new Point(0, 0, 0);
+			p2 = new Point(2, 0, 0);
+			result = sphere.findIntsersections(new Ray(new Vector(1, 0, 0),new Point(-1, 0, 0)));
+
+			assertEquals("Wrong number of points", 2, result.size());
+			if (result.get(0).getX() > result.get(1).getX())
+				result = List.of(result.get(1), result.get(0));
+			assertEquals("Ray crosses sphere", List.of(p1, p2), result);
+
+			// TC14: Ray starts at sphere and goes inside (1 points)
+			result = sphere.findIntsersections(new Ray(new Vector(-1, 0, 0),new Point(2, 0, 0)));
+			assertEquals("Wrong number of points", 1, result.size());
+			assertEquals("Ray starts at sphere and goes inside and cross in other place", Point.ZERO, result.get(0));
+
+			// TC15: Ray starts inside (1 points)
+			result = sphere.findIntsersections(new Ray(new Vector(1, 0, 0),new Point(1.5, 0, 0)));
+			assertEquals("Wrong number of points", 1, result.size());
+			assertEquals("The Ray does not cross in the right place", new Point(2, 0, 0), result.get(0));
+
+			// TC16: Ray starts at the center (1 points)
+			result = sphere.findIntsersections(new Ray(new Vector(1, 0, 0),new Point(1, 0, 0)));
+			assertEquals("Wrong number of points", 1, result.size());
+			assertEquals("The Ray does not cross in the right place", new Point(2, 0, 0), result.get(0));
+
+			// TC17: Ray starts at sphere and goes outside (0 points)
+			assertNull("Ray starts at sphere and goes outside and cross ???",
+					sphere.findIntsersections(new Ray(new Vector(1, 0, 0),new Point(2, 0, 0))));
+
+			// TC18: Ray starts after sphere (0 points)
+			assertNull("Ray's line need to be after of sphere",
+					sphere.findIntsersections(new Ray(new Vector(1, 0, 0),new Point(3, 0, 0))));
+
+			// **** Group: Ray's line is tangent to the sphere (all tests 0 points)
+			// TC19: Ray starts before the tangent point
+			assertNull("Ray starts before the tangent point and not cross the sphere",
+					sphere.findIntsersections(new Ray(new Vector(1, 0, 0),new Point(0, 1, 0))));
+
+			// TC20: Ray starts at the tangent point
+			assertNull("Ray starts at the tangent point and not cross the sphere",
+					sphere.findIntsersections(new Ray(new Vector(1, 0, 0),new Point(1, 1, 0))));
+
+			// TC21: Ray starts after the tangent point
+			assertNull("Ray starts after the tangent point and not cross the sphere",
+					sphere.findIntsersections(new Ray( new Vector(1, 0, 0),new Point(2, 1, 0))));
+
+			// **** Group: Special cases
+			// TC19: Ray's line is outside, ray is orthogonal to ray start to sphere's
+			// center line
+			assertNull("Ray's line is outside, ray is orthogonal to ray start to sphere's center line",
+					sphere.findIntsersections(new Ray( new Vector(0, 1, 0),new Point(-0.5, 0, 0))));
+		}
 	}
 
 }
+
